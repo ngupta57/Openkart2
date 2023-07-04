ff9164a05dc443ad6e992c02a9ba85e2eafb6731
Narinder Gupta <narinder.gupta@gmail.com>
2023-07-04 16:36:48 -0400

    COMMIT ADDED LOGIC INTHE REPORTING FILE UNDER UTILS TO GENERATE A RESULT FILE WITH TIME STAMP

--- openkart2/src/main/java/utils/Reporting.java
+++ openkart2/src/main/java/utils/Reporting.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
@@ -7,14 +10,19 @@ import com.relevantcodes.extentreports.LogStatus;
 public abstract class Reporting {
 
 	public static ExtentReports report;
+	public String repName;
 	
 	public static ExtentTest test;
+	
 	
 	public String testCaseName, testCaseDescription;
 	public String author, category;	
 	
-	public void startReport(){		
-		 report = new ExtentReports("./reports/Result2.html",false); 		
+	public void startReport(){	
+		 String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());// time stamp
+		 String repName = "Test-Report-" + timeStamp + ".html";
+		
+		 report = new ExtentReports("./reports/"+ repName); 		
 	}	
 	public void startTest(String testName,String description) {		
 		test = report.startTest(testName, description);
